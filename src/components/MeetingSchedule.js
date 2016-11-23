fcc75ea92361f47e551d2c1124bf39d526c76a9d
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2016-11-23 11:25:54 +0100

    Add copy first row button

--- src/components/MeetingSchedule.js
+++ src/components/MeetingSchedule.js
@@ -13,6 +13,24 @@ class MeetingSchedule extends Component {
     this.props.onUpdateSchedule(day, from, to);
   }
 
+  isFirstRowFilled() {
+    let { schedule } = this.props;
+    if (schedule.length === 0) {
+      return false;
+    }
+    return schedule[0].from !== undefined && schedule[0].to !== undefined &&
+      schedule[0].from.length > 0 && schedule[0].to.length > 0;
+  }
+
+  copyFirstRow() {
+    let { schedule } = this.props;
+    if (schedule.length === 0) {
+      return false;
+    }
+    let { from, to } = schedule[0];
+    schedule.forEach((event) => this.handleUpdate(event, from, to));
+  }
+
   sortDates(a, b){
     return a.day.valueOf() - b.day.valueOf();
   }
@@ -22,7 +40,11 @@ class MeetingSchedule extends Component {
     return (
       <fieldset className="MeetingSchedule">
         <legend>Przedziały godzin</legend>
-        {schedule.length === 0 && <p className="px-2 m-0 float-xs-left" style={{lineHeight: '1.8rem'}}>Wybierz dni dla spotkania</p>}
+        <button className="btn btn-sm btn-secondary btn-block" type="button" tabIndex="-1"
+                disabled={!this.isFirstRowFilled()} onClick={() => this.copyFirstRow()}>Kopiuj pierwszy wiersz</button>
+        {schedule.length === 0 && <p className="px-2 m-0 float-xs-left" style={{lineHeight: '1.8rem'}}>
+          Wybierz dni dla spotkania
+        </p>}
         {schedule.sort(this.sortDates).map((event) =>
           <MeetingScheduleEntry key={event.day.valueOf()} event={event} onDayRemove={() => onDayRemove(event.day)}
                                 onUpdate={this.handleUpdate.bind(this, event)} />
