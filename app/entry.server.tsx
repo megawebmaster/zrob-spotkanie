d5534d5ef044e6ec330002e12b442ed9cef148c2
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-01-26 18:29:28 +0100

    Fix i18next imports

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -6,7 +6,7 @@ import { RemixServer } from '@remix-run/react';
 import { renderToPipeableStream } from 'react-dom/server';
 import { createInstance } from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import Backend from 'i18next-fs-backend';
+import Backend from 'i18next-fs-backend/cjs';
 
 import i18next from './i18next.server';
 import i18n from './i18n';
--- app/i18next.server.ts
+++ app/i18next.server.ts
@@ -1,9 +1,10 @@
-import Backend from 'i18next-fs-backend';
-import { resolve } from 'node:path';
+import Backend from 'i18next-fs-backend/cjs';
 import { RemixI18Next } from 'remix-i18next/server';
+import { resolve } from 'node:path';
+
 import i18n from '~/i18n';
 
-let i18next = new RemixI18Next({
+const  i18next = new RemixI18Next({
   detection: {
     supportedLanguages: i18n.supportedLngs,
     fallbackLanguage: i18n.fallbackLng,
